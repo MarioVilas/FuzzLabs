dfd52c2ce55b511009cc00771864515a9d9c5cea
Ádám Gólya <adam.stork@gmail.com>
2015-10-13 21:35:46 +0200

    feat(setup): add node and bower to setup
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- webserver/setup.sh
+++ webserver/setup.sh
@@ -3,7 +3,7 @@
 # Essentials
 apt-get update
 apt-get upgrade
-apt-get install python-pip python-sqlite python-dev libffi-dev libxml2-dev libxslt1-dev lib32z1-dev libssl-dev
+apt-get install python-pip python-sqlite python-dev libffi-dev libxml2-dev libxslt1-dev lib32z1-dev libssl-dev curl
 pip install -r requirements.txt
 
 mkdir ./etc/ssl
@@ -14,3 +14,8 @@ rm ./etc/ssl/server.pass.key
 openssl req -new -key ./etc/ssl/server.key -out ./etc/ssl/server.csr
 openssl x509 -req -days 365 -in ./etc/ssl/server.csr -signkey ./etc/ssl/server.key -out ./etc/ssl/server.crt
 
+
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
+apt-get install --yes nodejs
+npm install -g bower
+bower install --allow-root
