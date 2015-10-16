c4abe3bbe19eba13924aa38486053f66530904a2
Keyman <zsolt.imre@dcnws.com>
2015-10-16 08:13:13 +0100

    Cleaned up old webserver database directory

--- webserver/setup.sh
+++ webserver/setup.sh
@@ -7,7 +7,6 @@ apt-get install python-pip python-sqlite python-dev libffi-dev libxml2-dev libxs
 pip install -r requirements.txt
 
 mkdir ./etc/ssl
-mkdir ./etc/database
 openssl genrsa -des3 -passout pass:x -out ./etc/ssl/server.pass.key 2048
 openssl rsa -passin pass:x -in ./etc/ssl/server.pass.key -out ./etc/ssl/server.key
 rm ./etc/ssl/server.pass.key
