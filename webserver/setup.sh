7df2241dfc5d54274a0d922bf45e047bc242b0bd
Keyman <zsolt.imre@dcnws.com>
2015-10-17 05:24:07 +0100

    Moved bower.json to webserver directory. Removed the creation of database directory from setup script. Updated bower.json.

--- webserver/setup.sh
+++ webserver/setup.sh
@@ -7,13 +7,11 @@ apt-get install python-pip python-sqlite python-dev libffi-dev libxml2-dev libxs
 pip install -r requirements.txt
 
 mkdir ./etc/ssl
-mkdir ./etc/database
 openssl genrsa -des3 -passout pass:x -out ./etc/ssl/server.pass.key 2048
 openssl rsa -passin pass:x -in ./etc/ssl/server.pass.key -out ./etc/ssl/server.key
 rm ./etc/ssl/server.pass.key
 openssl req -new -key ./etc/ssl/server.key -out ./etc/ssl/server.csr
 openssl x509 -req -days 365 -in ./etc/ssl/server.csr -signkey ./etc/ssl/server.key -out ./etc/ssl/server.crt
-
 
 curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install --yes nodejs
