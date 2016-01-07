f7738b1d713b222d46a8b6b48e970552e3248325
keymandll <zsolt.imre@dcnws.com>
2016-01-07 13:46:50 -0800

    Added checks to setup scripts.

--- engine/setup.sh
+++ engine/setup.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-# Essentials
+if [[ $(/usr/bin/id -u) -ne 0 ]] ; then
+    echo "[e] Must be root to execute setup, exiting..."
+    exit
+fi
+
+if [[ $(pwd | awk -F\/ '{ print $NF }') != "engine" ]] ; then
+    echo "[e] Please execute setup.sh from the engine directory, exiting..."
+    exit
+fi
+
 apt-get update
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to update package repository, exiting..."
+    exit
+fi
+
 apt-get upgrade
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to update system, exiting..."
+    exit
+fi
+
 apt-get install python-pip python-sqlite python-dev libbluetooth-dev libffi-dev libxml2-dev libxslt1-dev lib32z1-dev libssl-dev mongodb python-pymongo google-mock
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to install dependencies using apt-get, exiting..."
+    exit
+fi
+
 pip install -r requirements.txt
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to install dependencies using pip, exiting..."
+    exit
+fi
 
