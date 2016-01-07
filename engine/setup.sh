04d2db8c5508ead642aa134d21e3005732a6e26d
keymandll <zsolt.imre@dcnws.com>
2016-01-07 15:14:49 -0800

    Updated setup script, requirements.txt and the init script. Init script is now set up as part of the setup process.

--- engine/setup.sh
+++ engine/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DEFAULT_INSTALL_DIR="/opt/FuzzLabs/engine"
+SELECTED_INSTALL_DIR=${DEFAULT_INSTALL_DIR}
+
 if [[ $(/usr/bin/id -u) -ne 0 ]] ; then
     echo "[e] Must be root to execute setup, exiting..."
     exit
@@ -33,4 +36,52 @@ if [[ $? -ne 0 ]] ; then
     echo "[e] Failed to install dependencies using pip, exiting..."
     exit
 fi
+
+if [[ $(pwd) != ${DEFAULT_INSTALL_DIR} ]] ; then
+    echo -n "Install engine to ${DEFAULT_INSTALL_DIR}? [Y/n] "
+    read CHOICE
+    case ${CHOICE} in
+        n|N)
+            echo -n "Enter full path to install engine to: "
+            read SELECTED_INSTALL_DIR
+            ;;
+    esac
+    mkdir -p ${SELECTED_INSTALL_DIR}
+    cp -R ./* ${SELECTED_INSTALL_DIR}
+fi
+
+cat "$(pwd)/etc/init.rc.sh" | sed "s@{{{ENGINE_HOME}}}@$SELECTED_INSTALL_DIR@" > /etc/init.d/engine
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to setup init script, exiting..."
+    exit
+fi
+
+chmod 700 /etc/init.d/engine
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to setup init script, exiting..."
+    exit
+fi
+
+ln -s /etc/init.d/engine /etc/rc2.d/S99engine.sh
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to setup init script, exiting..."
+    exit
+fi
+
+echo -n "Start engine now? [Y/n] "
+read CHOICE
+case ${CHOICE} in
+    n|N)
+        exit 0
+        ;;
+esac
+
+echo "Starting engine..."
+/etc/init.d/engine start
+if [[ $? -ne 0 ]] ; then
+    echo "[e] Failed to start engine, exiting..."
+    exit 4
+fi
+echo "[e] Engine started successfully."
+exit 0
 
